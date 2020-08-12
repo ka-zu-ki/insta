a3117eabd0add8a81454db7d3e23fcc4c6ef50ed
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-08-12 18:20:26 +0900

    jsで画像表示変更

---
+++ app/javascript/packs/post.js
@@ -0,0 +1,27 @@
+import $ from 'jquery'
+// import axios from 'modules/axios'
+
+
+document.addEventListener('turbolinks:load', () => {
+  $('.profile_avatar').on('click', () => {
+    $('.profile_avatar_edit').removeClass('hidden')
+    $('profile_avatar').addClass('hidden')
+  })
+
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      
+      reader.onload = function (e) {
+        $('#avatar_img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+ 
+  $("#post_img").change(function(){
+    $('#avatar_img_prev').removeClass('hidden');
+    $('.avatar_present_img').remove();
+    readURL(this);
+  });
+})
