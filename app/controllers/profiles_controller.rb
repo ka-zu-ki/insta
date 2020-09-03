7df18bef74072cc0be12d770f1c698972d1c7179
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-03 14:33:44 +0900

    データベースをpgに変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,7 +18,7 @@ class ProfilesController < ApplicationController
   def update
     @profile = current_user.prepare_profile
     @profile.assign_attributes(profile_params)
-    if @profile.save
+    if @profile.save!
       redirect_to profile_path, notice: 'プロフィールを更新しました'
     else
       flash.now[:error] = '更新できませんでした'
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,13 +2,12 @@
 #
 # Table name: comments
 #
-#  id               :integer          not null, primary key
-#  commentable_type :string
-#  content          :text             not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  post_id          :integer          not null
-#  user_id          :integer          not null
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#  user_id    :bigint           not null
 #
 # Indexes
 #
